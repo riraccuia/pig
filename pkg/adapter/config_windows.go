444f45af75a71687a79661a5306af010734ab70e
riraccuia <riraccuia@gmail.com>
2025-03-12 09:42:47 +0100

    windows: replace waitForIPAddressReady with a sleep until fixed

--- pkg/adapter/config_windows.go
+++ pkg/adapter/config_windows.go
@@ -188,10 +188,11 @@ func setIPAddressUnicast(ifIndex int, ip net.IP, prefixLength uint8) error {
 		return fmt.Errorf("setIPAddressUnicast: CreateUnicastIpAddressEntry failed (ret: %d): %v", ret, err)
 	}
 
+	time.Sleep(time.Second)
 	// Wait for the IP address to be ready (with a 5-second timeout)
-	if err := waitForIPAddressReady(ifIndex, ip, 5*time.Second); err != nil {
-		return fmt.Errorf("setIPAddressUnicast: %v", err)
-	}
+	// if err := waitForIPAddressReady(ifIndex, ip, 5*time.Second); err != nil {
+	// 	return fmt.Errorf("setIPAddressUnicast: %v", err)
+	// }
 
 	return nil
 }
