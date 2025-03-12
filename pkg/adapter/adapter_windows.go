06aa8ea442a6a05df94b264cfef115d4cc73dea6
riraccuia <riraccuia@gmail.com>
2025-03-12 10:51:11 +0100

    windows: start with wintun device implementation from wireguard-go

--- pkg/adapter/adapter_windows.go
+++ pkg/adapter/adapter_windows.go
@@ -5,87 +5,30 @@ import (
 	"net"
 
 	"github.com/riraccuia/pig/pkg/common"
-	"golang.org/x/sys/windows"
-	"golang.zx2c4.com/wintun"
 )
 
 const tunName = "pig"
 
-type WinTunAdapter struct {
-	adapter *wintun.Adapter
-	session *wintun.Session
-	ip      net.IP
-}
-
 func newAdapter(config AdapterConfig) (common.TunnelAdapter, error) {
 	//wintun.SetLogger(nil)
 
-	adapter, err := wintun.CreateAdapter(tunName, "pig", &windows.GUID{
-		Data1: 0x0000000,
-		Data2: 0x0000,
-		Data3: 0x0000,
-		Data4: [8]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
-	})
+	adapter, err := CreateTUNWithRequestedGUID(tunName, WintunStaticRequestedGUID)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create wintun adapter: %v", err)
 	}
 
-	session, err := adapter.StartSession(0x800000) // 8MB ring buffer
-	if err != nil {
-		adapter.Close()
-		return nil, fmt.Errorf("failed to start wintun session: %v", err)
-	}
-
 	ip, _, err := net.ParseCIDR(config.Address)
 	if err != nil {
-		session.End()
 		adapter.Close()
 		return nil, fmt.Errorf("failed to parse IP: %v", err)
 	}
 
-	w := &WinTunAdapter{
-		adapter: adapter,
-		session: &session,
-		ip:      ip,
-	}
+	adapter.ip = ip
 
 	if err := configureWinTun(tunName, config); err != nil {
-		w.Close()
+		adapter.Close()
 		return nil, fmt.Errorf("failed to configure adapter: %v", err)
 	}
 
-	return w, nil
-}
-
-func (w *WinTunAdapter) Read(b []byte) (int, error) {
-	packet, err := w.session.ReceivePacket()
-	if err != nil {
-		return 0, err
-	}
-	n := copy(b, packet)
-	w.session.ReleaseReceivePacket(packet)
-	return n, nil
-}
-
-func (w *WinTunAdapter) Write(b []byte) (int, error) {
-	packet, err := w.session.AllocateSendPacket(len(b))
-	if err != nil {
-		return 0, err
-	}
-	copy(packet, b)
-	w.session.SendPacket(packet)
-	return len(b), nil
-}
-
-func (w *WinTunAdapter) Close() error {
-	w.session.End()
-	return w.adapter.Close()
-}
-
-func (w *WinTunAdapter) IP() net.IP {
-	return w.ip
-}
-
-func (w *WinTunAdapter) Name() string {
-	return tunName
+	return adapter, nil
 }
---
+++ pkg/adapter/tun_windows.go
@@ -0,0 +1,211 @@
+//go:build windows
+// +build windows
+
+package adapter
+
+import (
+	"errors"
+	"fmt"
+	"net"
+	"os"
+	"sync"
+	"sync/atomic"
+	"time"
+
+	"golang.org/x/sys/windows"
+	"golang.zx2c4.com/wintun"
+)
+
+const (
+	rateMeasurementGranularity = uint64((time.Second / 2) / time.Nanosecond)
+	spinloopRateThreshold      = 800000000 / 8                                   // 800mbps
+	spinloopDuration           = uint64(time.Millisecond / 80 / time.Nanosecond) // ~1gbit/s
+)
+
+type rateJuggler struct {
+	current       atomic.Uint64
+	nextByteCount atomic.Uint64
+	nextStartTime atomic.Int64
+	changing      atomic.Bool
+}
+
+type NativeTun struct {
+	ip        net.IP
+	wt        *wintun.Adapter
+	name      string
+	handle    windows.Handle
+	rate      rateJuggler
+	session   wintun.Session
+	readWait  windows.Handle
+	events    chan Event
+	running   sync.WaitGroup
+	closeOnce sync.Once
+	close     atomic.Bool
+	outSizes  []int
+}
+
+type Event int
+
+const (
+	EventMTUUpdate Event = iota
+)
+
+var (
+	WintunTunnelType          = "pig"
+	WintunStaticRequestedGUID *windows.GUID
+)
+
+//go:linkname procyield runtime.procyield
+func procyield(cycles uint32)
+
+//go:linkname nanotime runtime.nanotime
+func nanotime() int64
+
+// CreateTUNWithRequestedGUID creates a Wintun interface with the given name and
+// a requested GUID. Should a Wintun interface with the same name exist, it is reused.
+func CreateTUNWithRequestedGUID(ifname string, requestedGUID *windows.GUID) (*NativeTun, error) {
+	wt, err := wintun.CreateAdapter(ifname, WintunTunnelType, requestedGUID)
+	if err != nil {
+		return nil, fmt.Errorf("Error creating interface: %w", err)
+	}
+
+	tun := &NativeTun{
+		wt:     wt,
+		name:   ifname,
+		handle: windows.InvalidHandle,
+		events: make(chan Event, 10),
+	}
+
+	tun.session, err = wt.StartSession(0x800000) // Ring capacity, 8 MiB
+	if err != nil {
+		tun.wt.Close()
+		close(tun.events)
+		return nil, fmt.Errorf("Error starting session: %w", err)
+	}
+	tun.readWait = tun.session.ReadWaitEvent()
+	return tun, nil
+}
+
+func (tun *NativeTun) Name() string {
+	return tun.name
+}
+
+func (tun *NativeTun) File() *os.File {
+	return nil
+}
+
+func (tun *NativeTun) Events() <-chan Event {
+	return tun.events
+}
+
+func (tun *NativeTun) IP() net.IP {
+	return tun.ip
+}
+
+func (tun *NativeTun) Close() error {
+	var err error
+	tun.closeOnce.Do(func() {
+		tun.close.Store(true)
+		windows.SetEvent(tun.readWait)
+		tun.running.Wait()
+		tun.session.End()
+		if tun.wt != nil {
+			tun.wt.Close()
+		}
+		close(tun.events)
+	})
+	return err
+}
+
+// Note: Read() and Write() assume the caller comes only from a single thread; there's no locking.
+
+func (tun *NativeTun) Read(buf []byte) (int, error) {
+	tun.running.Add(1)
+	defer tun.running.Done()
+retry:
+	if tun.close.Load() {
+		return 0, os.ErrClosed
+	}
+	start := nanotime()
+	shouldSpin := tun.rate.current.Load() >= spinloopRateThreshold && uint64(start-tun.rate.nextStartTime.Load()) <= rateMeasurementGranularity*2
+	for {
+		if tun.close.Load() {
+			return 0, os.ErrClosed
+		}
+		packet, err := tun.session.ReceivePacket()
+		switch err {
+		case nil:
+			n := copy(buf, packet)
+			tun.session.ReleaseReceivePacket(packet)
+			tun.rate.update(uint64(n))
+			return n, nil
+		case windows.ERROR_NO_MORE_ITEMS:
+			if !shouldSpin || uint64(nanotime()-start) >= spinloopDuration {
+				windows.WaitForSingleObject(tun.readWait, windows.INFINITE)
+				goto retry
+			}
+			procyield(1)
+			continue
+		case windows.ERROR_HANDLE_EOF:
+			return 0, os.ErrClosed
+		case windows.ERROR_INVALID_DATA:
+			return 0, errors.New("Send ring corrupt")
+		}
+		return 0, fmt.Errorf("Read failed: %w", err)
+	}
+}
+
+func (tun *NativeTun) Write(buf []byte) (int, error) {
+	tun.running.Add(1)
+	defer tun.running.Done()
+	if tun.close.Load() {
+		return 0, os.ErrClosed
+	}
+
+	tun.rate.update(uint64(len(buf)))
+
+	packet, err := tun.session.AllocateSendPacket(len(buf))
+	switch err {
+	case nil:
+		// TODO: Explore options to eliminate this copy.
+		copy(packet, buf)
+		tun.session.SendPacket(packet)
+	case windows.ERROR_HANDLE_EOF:
+		return 0, os.ErrClosed
+	case windows.ERROR_BUFFER_OVERFLOW:
+		return 0, nil // Dropped when ring is full.
+	default:
+		return 0, fmt.Errorf("Write failed: %w", err)
+	}
+	return len(buf), nil
+}
+
+// LUID returns Windows interface instance ID.
+func (tun *NativeTun) LUID() uint64 {
+	tun.running.Add(1)
+	defer tun.running.Done()
+	if tun.close.Load() {
+		return 0
+	}
+	return tun.wt.LUID()
+}
+
+// RunningVersion returns the running version of the Wintun driver.
+func (tun *NativeTun) RunningVersion() (version uint32, err error) {
+	return wintun.RunningVersion()
+}
+
+func (rate *rateJuggler) update(packetLen uint64) {
+	now := nanotime()
+	total := rate.nextByteCount.Add(packetLen)
+	period := uint64(now - rate.nextStartTime.Load())
+	if period >= rateMeasurementGranularity {
+		if !rate.changing.CompareAndSwap(false, true) {
+			return
+		}
+		rate.nextStartTime.Store(now)
+		rate.current.Store(total * uint64(time.Second/time.Nanosecond) / period)
+		rate.nextByteCount.Store(0)
+		rate.changing.Store(false)
+	}
+}
