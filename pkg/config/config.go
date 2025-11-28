b69c5e6293e9b0afbe60dcacc0d12160c320aed2
riraccuia <riraccuia@gmail.com>
2025-11-28 11:51:07 +0100

    add json tags to main config struct

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -84,37 +84,37 @@ func (m ICMPMode) IsValid() bool {
 }
 
 type Config struct {
-	Mode              Mode          `toml:"mode"`           // "client" or "server"
-	LogConfig         LogConfig     `toml:"log"`            // Log configuration
-	TunnelAddress     string        `toml:"tunnel_address"` // CIDR format
-	MTU               int           `toml:"mtu"`
-	CertFile          string        `toml:"cert_file"`
-	KeyFile           string        `toml:"key_file"`
-	StreamCount       int           `toml:"stream_count"`
-	Insecure          bool          `toml:"insecure"` // Skip TLS certificate verification if true
-	Proto             TransportType `toml:"proto"`
-	Target            Target        `toml:"target"`
-	ReconnectInterval int           `toml:"reconnect_interval"` // in seconds
-	BindAdapter       string        `toml:"bind_adapter"`       // The adapter to bind to
-	Wred              WredConfig    `toml:"wred"`
-	StartScript       string        `toml:"start_script"` // Script to execute when a tunnel connection is established
-	StopScript        string        `toml:"stop_script"`  // Script to execute when a tunnel connection is disconnected
-	Auth              *AuthConfig   `toml:"auth"`
-	QueueSize         int           `toml:"queue_size"` // Size of packet queues (default: 256)
-	ICE               *ICEConfig    `toml:"ice"`
+	Mode              Mode          `toml:"mode" json:"mode"`                     // "client" or "server"
+	LogConfig         LogConfig     `toml:"log" json:"log"`                       // Log configuration
+	TunnelAddress     string        `toml:"tunnel_address" json:"tunnel_address"` // CIDR format
+	MTU               int           `toml:"mtu" json:"mtu"`
+	CertFile          string        `toml:"cert_file" json:"cert_file"`
+	KeyFile           string        `toml:"key_file" json:"key_file"`
+	StreamCount       int           `toml:"stream_count" json:"stream_count"`
+	Insecure          bool          `toml:"insecure" json:"insecure"` // Skip TLS certificate verification if true
+	Proto             TransportType `toml:"proto" json:"proto"`
+	Target            Target        `toml:"target" json:"target"`
+	ReconnectInterval int           `toml:"reconnect_interval" json:"reconnect_interval"` // in seconds
+	BindAdapter       string        `toml:"bind_adapter" json:"bind_adapter"`             // The adapter to bind to
+	Wred              WredConfig    `toml:"wred" json:"wred"`
+	StartScript       string        `toml:"start_script" json:"start_script"` // Script to execute when a tunnel connection is established
+	StopScript        string        `toml:"stop_script" json:"stop_script"`   // Script to execute when a tunnel connection is disconnected
+	Auth              *AuthConfig   `toml:"auth" json:"auth"`
+	QueueSize         int           `toml:"queue_size" json:"queue_size"` // Size of packet queues (default: 256)
+	ICE               *ICEConfig    `toml:"ice" json:"ice"`
 }
 
 type LogConfig struct {
-	File       string `toml:"file"`        // Path to log file
-	Level      string `toml:"level"`       // Log level
-	RotateSize any    `toml:"rotate_size"` // e.g. 100k, 1m, 1g, or actual size in bytes
+	File       string `toml:"file" json:"file"`               // Path to log file
+	Level      string `toml:"level" json:"level"`             // Log level
+	RotateSize any    `toml:"rotate_size" json:"rotate_size"` // e.g. 100k, 1m, 1g, or actual size in bytes
 }
 
 type ICEConfig struct {
-	Enabled     bool              `toml:"enabled"`      // Enable ICE based hole punching
-	Protos      []string          `toml:"protos"`       // Protocols to use for ICE
-	STUNAddress string            `toml:"stun_address"` // STUN server address
-	Signaling   *ICESignalingOpts `toml:"signaling"`    // Signaling options
+	Enabled     bool              `toml:"enabled" json:"enabled"`           // Enable ICE based hole punching
+	Protos      []string          `toml:"protos" json:"protos"`             // Protocols to use for ICE
+	STUNAddress string            `toml:"stun_address" json:"stun_address"` // STUN server address
+	Signaling   *ICESignalingOpts `toml:"signaling" json:"signaling"`       // Signaling options
 }
 
 func (c *ICEConfig) UseProtos() (protos []transport.ICEProtocolDefinition) {
@@ -139,45 +139,45 @@ func (c *ICEConfig) UseProtos() (protos []transport.ICEProtocolDefinition) {
 }
 
 type ICESignalingOpts struct {
-	ServerID          string `toml:"server_id"`           // Use a connection ID to identify the connection, instead of the mapped IP
-	EncryptionKey     string `toml:"encryption_key"`      // Encryption key for the ICE signaling
-	ConnectOffset     int    `toml:"connect_offset"`      // Connect offset in milliseconds
-	MQTTBrokerAddress string `toml:"mqtt_broker_address"` // MQTT broker address in the form of mqtt://host:port or ssl://host:port
-	MQTTClientID      string `toml:"mqtt_client_id"`      // MQTT client ID
-	MQTTUsername      string `toml:"mqtt_username"`       // MQTT username
-	MQTTPassword      string `toml:"mqtt_password"`       // MQTT password
+	ServerID          string `toml:"server_id" json:"server_id"`                     // Use a connection ID to identify the connection, instead of the mapped IP
+	EncryptionKey     string `toml:"encryption_key" json:"encryption_key"`           // Encryption key for the ICE signaling
+	ConnectOffset     int    `toml:"connect_offset" json:"connect_offset"`           // Connect offset in milliseconds
+	MQTTBrokerAddress string `toml:"mqtt_broker_address" json:"mqtt_broker_address"` // MQTT broker address in the form of mqtt://host:port or ssl://host:port
+	MQTTClientID      string `toml:"mqtt_client_id" json:"mqtt_client_id"`           // MQTT client ID
+	MQTTUsername      string `toml:"mqtt_username" json:"mqtt_username"`             // MQTT username
+	MQTTPassword      string `toml:"mqtt_password" json:"mqtt_password"`             // MQTT password
 }
 
 type Target struct {
-	Address string `toml:"address"`
-	Port    int    `toml:"port"`
-	SrcPort int    `toml:"src_port"`
+	Address string `toml:"address" json:"address"`
+	Port    int    `toml:"port" json:"port"`
+	SrcPort int    `toml:"src_port" json:"src_port"`
 }
 
 type WredConfig struct {
-	WeightFactor    float64 `toml:"weight_factor"`
-	DropProbability float64 `toml:"drop_probability"`
-	Threshold       float64 `toml:"threshold"`
+	WeightFactor    float64 `toml:"weight_factor" json:"weight_factor"`
+	DropProbability float64 `toml:"drop_probability" json:"drop_probability"`
+	Threshold       float64 `toml:"threshold" json:"threshold"`
 }
 
 type AuthConfig struct {
-	Type AuthType    `toml:"type"`
-	JWT  *JWTAuth    `toml:"jwt"`
-	MTLS *MTLSConfig `toml:"mtls"`
+	Type AuthType    `toml:"type" json:"type"`
+	JWT  *JWTAuth    `toml:"jwt" json:"jwt"`
+	MTLS *MTLSConfig `toml:"mtls" json:"mtls"`
 }
 
 type MTLSConfig struct {
 	// TrustPEM is the path to the trust bundle for MTLS, if not provided, the system CA will be used
 	// in client mode, this is used to validate the server certificate
 	// in server mode, this is used to validate client certificates
-	TrustPEM string `toml:"trust_pem"`
+	TrustPEM string `toml:"trust_pem" json:"trust_pem"`
 }
 
 type JWTAuth struct {
 	// PublicKeySource is the path to the public key file for JWT verification
-	PublicKeySource string `toml:"public_key_source"`
+	PublicKeySource string `toml:"public_key_source" json:"public_key_source"`
 	// Token is the JWT token to use for authentication (client only)
-	Token string `toml:"token"`
+	Token string `toml:"token" json:"token"`
 }
 
 func LoadConfig(path string) (*Config, error) {
